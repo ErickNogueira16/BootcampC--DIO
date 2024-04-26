5fc071acd20654bcd48e183f578fbc9e43c567ba
Erick Nogueira Alvarenga <117676267+ErickNogueira16@users.noreply.github.com>
2024-04-26 16:40:54 -0300

    Adicionando terceiro desafio de código - Sistema de celular

---
+++ SistemaCelular/Program.cs
@@ -0,0 +1,12 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n = new Nokia("+5573999991111", "Lumia", "5647dfmsr3re231", 16);
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("WhatsApp");
+
+Iphone i = new Iphone("+5527999992222", "IPhone 11", "348UFJF342U3RFJ", 64);
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("WhatsApp");
